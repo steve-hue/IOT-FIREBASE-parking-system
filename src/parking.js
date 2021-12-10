ce4f89c801ff4a9d00691cb5402461a5f2a19070
Steve Stagy <stevesk2001@gmail.com>
2021-12-10 16:16:55 +0400

    first commit

---
+++ src/parking.js
@@ -0,0 +1,19 @@
+const parking=[
+    {
+        id:"P1"
+    },
+    {
+        id:"P2"
+    },
+    {
+        id:"P3"
+    },
+    {
+        id:"P4"
+    },
+    {
+        id:"P5"
+    }
+]
+
+export default parking
