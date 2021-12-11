461b3265a28147d2c78d0e0ca852937cf388b779
Sanskar Arora <55059942+sunny-1651@users.noreply.github.com>
2021-12-11 21:06:57 +0530

    Add files via upload

---
+++ WebApp/src/book.js
@@ -0,0 +1,26 @@
+const booking=[
+    {
+        name:"1",
+        slot:false
+    },
+    {
+        name:"2",
+        slot:false
+    },
+    {
+        name:"3",
+        slot:false
+    },
+    {
+        name:"4",
+        slot:false
+    },
+    {
+        name:"5",
+        slot:false
+    },
+
+]
+
+
+export default booking
